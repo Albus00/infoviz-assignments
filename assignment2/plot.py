80fc37b2a0c4b5d1be3295d166676e7c2610daa1
tovelito <extratove@hotmail.com>
2024-01-30 16:55:47 +0100

    Jag ändra allt

--- assignment2/plot.py
+++ assignment2/plot.py
@@ -9,22 +9,6 @@ window.title("Scatter Plot Visualization")
 canvas = tk.Canvas(window, width=700, height=700)
 canvas.pack()
 
-# Calculate the minimum and maximum values for x and y axes
-min_x = float('50')
-max_x = float('650')
-min_y = float('50')
-max_y = float('650')
-
-# Draw the x-axis
-canvas.create_line(50, 350, 650, 350)  # x-axis line
-# Draw the ticks and tick values for x-axis
-# ...
-
-# Draw the y-axis
-canvas.create_line(350, 50, 350, 650)  # y-axis line
-
-# Draw the ticks and tick values for y-axis
-# ...
 
 # Load and process the data using csvreader
 with open('data1.csv', 'r') as file:
@@ -34,17 +18,49 @@ with open('data1.csv', 'r') as file:
   # Read the header row
   header = next(csvreader)
 
-  # Process each row of data
-  for line in csvreader:
-    # Access the data in each column
-    xSet = float(line[0])
-    ySet = float(line[1])
-    letterSet = line[2]
-    print(xSet, ySet, letterSet)
-    x = 50 + (xSet - min_x) * (600 / (max_x - min_x))
-    y = 650 - (ySet - min_y) * (600 / (max_y - min_y))
-    canvas.create_oval(x - 3, y - 3, x + 3, y + 3, fill="blue")
+  # Load and process the data using csvreader
+  data = []  # List to store the data
 
+  # Initiate the minimum and maximum values for x and y axes
+  min_x = float('inf')
+  max_x = float('-inf')
+  min_y = float('inf')
+  max_y = float('-inf')
+
+  for line in csvreader:
+    x = float(line[0])
+    y = float(line[1])
+    max_x = max(max_x, x)
+    max_y = max(max_y, y)
+    min_x = min(min_x, x)
+    min_y = min(min_y, y)
+    data.append((x, y, line[2]))
+  print(min_x, max_x, min_y, max_y)
+
+    # Update maximum x and y values if needed
+  for entry in data:
+    x = ((entry[0] + max_x) / (2*max_x)) * 650
+    y = ((entry[1] + max_y) / (2*max_y)) * 650
+    letterSet = entry[2]
+    style_mapping = {'a': 'pink', 'b': 'blue', 'c': 'red'}
+    canvas.create_oval(x - 3, y - 3, x + 3, y + 3, fill=style_mapping.get(letterSet, 'blue'))
+
+
+canvas.create_line(50, 350, 650, 350)  # x-axis line
+canvas.create_line(350, 50, 350, 650)  # y-axis line
+
+# draw x axis and create text through iteration
+# draw x axis and create text through iteration
+for i in range(1, 9):
+    x_tick_position = (i / 8) * 650
+    canvas.create_line(x_tick_position, 350, x_tick_position, 345, width=2)
+    canvas.create_text(x_tick_position, 360, text=f'{min_x-i*((min_x-max_x)/ 8):.1f}', font=("Purisa", 10))
+
+# draw y axis and create text through iteration
+for i in range(1, 9):
+    y_tick_position = 700 - (i / 8) * 650
+    canvas.create_line(350, y_tick_position, 345, y_tick_position, width=2)
+    canvas.create_text(360, y_tick_position, text=f'{i * max_y / 8:.1f}', font=("Purisa", 10))
 
   # i = 0;
   # while i < len(xSet):
