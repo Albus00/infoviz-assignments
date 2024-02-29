cb66fa0490fc74029dafe4224757bc87d768dd6e
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2024-02-29 12:41:48 +0100

    Optimized to oblivion

--- assignment4/script.js
+++ assignment4/script.js
@@ -3,7 +3,6 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 // Declare the global data variable
 let data1;
 let data2;
-let episodesData = [];
 let leftEpisodes = [];
 let rightEpisodes = [];
 
@@ -16,27 +15,7 @@ let width = window.innerWidth / 2 - 5;
 let height = window.innerHeight - 20 - menuHeight;
 
 // Declare the layout
-let layout, layout2;
-
-// Create a function that imports the data from the all the episodes
-async function importAllEpisodes() {
-  for (let i = 1; i <= 7; i++) {
-    await import('./starwars-interactions/starwars-episode-' + i + '-interactions-allCharacters.json', {
-      assert: { type: 'json' }
-    }).then(({ default: data }) => {
-      episodesData.push(data);
-    });
-  }
-
-  episodesData.forEach((episode, index) => {
-    // connect the links to the nodes
-    episode.links.forEach(link => {
-      link.source = episode.nodes[link.source];
-      link.target = episode.nodes[link.target];
-    });
-  });
-
-}
+let layout = d3.forceSimulation()
 
 // Create a function that fetches the links from a selected episode
 async function fetchEpisode(episode) {
@@ -52,7 +31,7 @@ async function fetchEpisode(episode) {
     );
 }
 
-//get checked episodes
+//get checked episodes from the first selector
 episodeSelector1.on("change", (event) => {
   let checkedEpisodes = [];
   d3.selectAll(".l-episode").each(function (d) {
@@ -71,6 +50,7 @@ episodeSelector1.on("change", (event) => {
   getEpisode("left", leftEpisodes);
 });
 
+//get checked episodes from the second selector
 episodeSelector2.on("change", (event) => {
   let checkedEpisodes = [];
   d3.selectAll(".r-episode").each(function (d) {
@@ -89,47 +69,53 @@ episodeSelector2.on("change", (event) => {
   getEpisode("right", rightEpisodes);
 });
 
+// Change the window width and height when the window is resized
+window.onresize = function () {
+  width = window.innerWidth / 2 - 5;
+  height = window.innerHeight - 20 - menuHeight;
+  svg1.attr("width", width / 2).attr("height", height);
+  svg2.attr("width", width / 2).attr("height", height);
+
+  // Update the layout
+  updateCanvas(data1, svg1);
+  updateCanvas(data2, svg2);
+
+};
+
 // Import data from selected episode
 function getEpisode(field, episodes) {
-  let episodeFetch = new Promise((resolve, reject) => {
+  let dataFetch = new Promise((resolve, reject) => {
     episodes.forEach(async (episode, index) => {
       await fetchEpisode(episode).then(episode => {
-        if (field === "left") {
-          data1 = updateDataset(data1, episode);
-        } else {
-          data2 = updateDataset(data2, episode);
-        }
+        field === "left" ?
+          data1 = updateDataset(data1, episode) :
+          data2 = updateDataset(data2, episode)
       });
+      // Check if the last episode has been fetched then resolve the promise
       index == episodes.length - 1 ? resolve() : null;
     })
   });
 
-  episodeFetch.then(() => {
-    console.log("RESOLVE: ", data1);
-    if (field === "left") {
-      svg1.selectAll("*").remove(); // Clear the svg1
-      updateCanvas1(data1);         // Update the dataset for svg1
-    } else if (field === "right") {
-      svg2.selectAll("*").remove(); // Clear the svg2
-      updateCanvas2(data2);         // Update the dataset for svg2
-    }
+  dataFetch.then(() => {
+    field === "left" ? updateCanvas(data1, svg1) : updateCanvas(data2, svg2);
   });
 }
 
 // Add new data to the dataset
 function updateDataset(data, addedData) {
   // Create a copy of the data
-  let addedNodes = [...addedData.nodes];
+  let addedNodes = addedData.nodes;
   let addedLinks = addedData.links;
 
+  // Check if this is the first dataset to be added
   if (data === undefined) {
-    return { nodes: addedNodes, links: addedLinks };
+    return addedData;
   }
   addedNodes.forEach(addNode => {
     data.nodes.forEach(node => {
+      // Check if the character already exists in the dataset
       if (node.name == addNode.name) {
-        // Character already exists in the dataset and needs to be updated
-        // console.log("updated: ", node.name, addNode.name);
+        // Update edge value of the character
         node.value += addNode.value;
 
         // Update the links to the character
@@ -147,101 +133,33 @@ function updateDataset(data, addedData) {
     });
   });
 
+  // Add the new and filtered data to the dataset
   data.nodes = data.nodes.concat(addedNodes);
   data.links = data.links.concat(addedLinks);
   return data;
 }
 
-// Write a function that highlights the node from another network when hovering over a node, based on the class name
-function highlightNode(nodeName, toHighlight) {
-  let className = formatClassName(nodeName);
-  if (toHighlight) {
-    // Highlight the links connected to the node
-    highlightLinks(nodeName, true);
-
-    // Give the node a border and make it bigger
-    svg1.selectAll("." + className)
-      .attr("stroke", "black")
-      .attr("stroke-width", 2)
-      .attr("r", 15);
-
-    svg2.selectAll("." + className)
-      .attr("stroke", "black")
-      .attr("stroke-width", 2)
-      .attr("r", 15);
-  }
-  else {
-    highlightLinks(nodeName, false);
-
-    // Remove the border and make the node smaller
-    svg1.selectAll("." + className)
-      .attr("stroke", "none")
-      .attr("r", 10);
-
-    svg2.selectAll("." + className)
-      .attr("stroke", "none")
-      .attr("r", 10);
-  }
-}
-
-// Create a function that highlights the links connected to a node
-function highlightLinks(nodeName, toHighlight) {
-  if (data1 !== undefined) {
-    // Loop through each link
-    data1.links.forEach(link => {
-      // Check if the link is connected to the specified node
-      if (link.source.name === nodeName || link.target.name === nodeName) {
-        //console.log("link: ", link.target.name, nodeName);
-        // go through all the classes that contains the words link_ and the source and target names
-        let className = ".link_" + formatClassName(link.source.name) + "_" + formatClassName(link.target.name);
-        if (toHighlight) {
-          // Highlight the link
-          svg1.selectAll(className)
-            .attr("stroke", "red")
-            .attr("stroke-width", 2);
-        }
-        else {
-          // Remove the highlight from the link
-          svg1.selectAll(className)
-            .attr("stroke", "black")
-            .attr("stroke-width", 1);
-        }
-      }
-    });
-  }
-  if (data2 !== undefined) {
-    // Loop through each link
-    data2.links.forEach(link => {
-      // Check if the link is connected to the specified node
-      if (link.source.name === nodeName || link.target.name === nodeName) {
-        console.log("link: ", link.source.name, nodeName);
-        // go through all the classes that contains the words link_ and the source and target names
-        let className = ".link_" + formatClassName(link.source.name) + "_" + formatClassName(link.target.name);
-        if (toHighlight) {
-          // Highlight the link
-          svg2.selectAll(className)
-            .attr("stroke", "red")
-            .attr("stroke-width", 2);
-        }
-        else {
-          // Remove the highlight from the link
-          svg2.selectAll(className)
-            .attr("stroke", "black")
-            .attr("stroke-width", 1);
-        }
-      }
-    });
-  }
-}
-
-// Create a function that turns a string into lowercase and replaces all spaces with -
+// Function that turns a string into lowercase and replaces all spaces with -
 function formatClassName(str) {
   return str.toLowerCase().replace(/ /g, "-");
 }
 
+// Create a function that checks if name is in dataset
+function checkName(name, data) {
+  let result = false;
+  if (data === undefined) {
+    return result;
+  }
+  data.nodes.forEach(node => {
+    if (node.name === name) {
+      result = node;
+    }
+  });
+  return result;
+}
+
 // Create a function that finds the node in the data set and returns which characters it interacts with
 function findInteractions(node, data) {
-  console.log("node: ", node);
   let interactions = [];
   data.links.forEach(link => {
     if (link.source.name === node.name) {
@@ -254,51 +172,83 @@ function findInteractions(node, data) {
   return interactions;
 }
 
-// Create a function that checks if name is in dataset
-function checkName(name, data) {
-  let result = false;
-  data.nodes.forEach(node => {
-    if (node.name === name) {
-      result = node;
-    }
-  });
-  return result;
+// Write a function that highlights the node from another network when hovering over a node, based on the class name
+function highlightNode(nodeName, toHighlight) {
+  let className = formatClassName(nodeName);
+  let svgArray = [svg1, svg2];
+  if (toHighlight) {
+    // Highlight the links connected to the node
+    highlightLinks(nodeName, true);
+
+    // Give the node a border and make it bigger
+    svgArray.forEach(svg => {
+      svg.selectAll("." + className)
+        .attr("stroke", "black")
+        .attr("stroke-width", 2)
+        .attr("r", 15);
+    });
+  }
+  else {
+    highlightLinks(nodeName, false);
+
+    svgArray.forEach(svg => {
+      svg.selectAll("." + className)
+        .attr("stroke", "none")
+        .attr("r", 10);
+    });
+  }
+}
+
+// Function that highlights the links connected to a node
+function highlightLinks(nodeName, toHighlight) {
+  const highlight = (svg, data) => {
+    data.links.forEach(link => {
+      if (link.source.name === nodeName || link.target.name === nodeName) {
+        // Match the class name format when searching for the lines
+        const className = ".link_" + formatClassName(link.source.name) + "_" + formatClassName(link.target.name);
+        svg.selectAll(className)
+          .attr("stroke", toHighlight ? "red" : "black")
+          .attr("stroke-width", toHighlight ? 2 : 1);
+      }
+    });
+  };
+
+  // Highlight the links in both networks
+  data1 !== undefined ? highlight(svg1, data1) : null;
+  data2 !== undefined ? highlight(svg2, data2) : null;
 }
 
 function generateTooltips(node) {
+  const generateTooltipsContent = (char, interactions) => {
+    let tooltipContent = "<div><h3>" + char.name + "</h3><h3>" + char.value + "</h3></div>";
+    interactions.forEach(interaction => {
+      tooltipContent += "<div><p>" + interaction[0] + "</p><p>" + interaction[1] + "</p></div>";
+    });
+    return tooltipContent;
+  }
+
   // Left network
-  let height = 5;
-  let leftTooltip = document.getElementById("tooltips-left");
   let char;
+  let tooltipHeight = 5;
+  let tooltipContainer = document.getElementById("tooltips-left");
+  tooltipContainer.innerHTML = "";
   if (char = checkName(node.name, data1)) {
     let interactions = findInteractions(char, data1);
-    leftTooltip.innerHTML = "<div><h3>" + char.name + "</h3><h3>" + char.value + "</h3></div>";
-    interactions.forEach(interaction => {
-      leftTooltip.innerHTML += "<div><p>" + interaction[0] + "</p><p>" + interaction[1] + "</p></div>";
-      height += 0.75;
-      console.log("height: ", height);
-    });
+    tooltipContainer.innerHTML = generateTooltipsContent(char, interactions);
+    tooltipHeight += interactions.length * 0.8;
   }
-  else {
-    leftTooltip.innerHTML = "";
-  }
-  leftTooltip.style.height = height + "rem";
+  tooltipContainer.style.height = tooltipHeight + "rem";
 
   // Right network
-  height = 5;
-  let rightTooltip = document.getElementById("tooltips-right");
+  tooltipHeight = 5;
+  tooltipContainer = document.getElementById("tooltips-right");
+  tooltipContainer.innerHTML = "";
   if (char = checkName(node.name, data2)) {
     let interactions = findInteractions(char, data2);
-    rightTooltip.innerHTML = "<div><h3>" + char.name + "</h3><h3>" + char.value + "</h3></div>";
-    interactions.forEach(interaction => {
-      rightTooltip.innerHTML += "<div><p>" + interaction[0] + "</p><p>" + interaction[1] + "</p></div>";
-      height += 0.75;
-    });
+    tooltipContainer.innerHTML = generateTooltipsContent(char, interactions);
+    tooltipHeight += interactions.length * 0.8;
   }
-  else {
-    rightTooltip.innerHTML = "";
-  }
-  rightTooltip.style.height = height + "rem";
+  tooltipContainer.style.height = tooltipHeight + "rem";
 }
 
 
@@ -329,7 +279,7 @@ function createNodes(nodes_data, svg) {
 }
 
 // Create the link lines
-function createLinks(links_data, svg, isHighlighted) {
+function createLinks(links_data, svg) {
   return svg.selectAll(".link")
     .data(links_data)
     .enter().append("line")
@@ -337,35 +287,13 @@ function createLinks(links_data, svg, isHighlighted) {
     .attr("stroke", "black");
 }
 
-// Change the window width and height when the window is resized
-window.onresize = function () {
-  width = window.innerWidth / 2 - 5;
-  height = window.innerHeight - 20 - menuHeight;
-  svg1.attr("width", width / 2).attr("height", height);
-  svg2.attr("width", width / 2).attr("height", height);
+function updateCanvas(data, svg) {
+  // Clear the svg
+  svg.selectAll("*").remove();
 
-  // Change the layout
-  layout
-    .force("center", d3.forceCenter().x(width / 2).y(height / 2))
-    .restart();
-  layout2
-    .force("center", d3.forceCenter().x(width / 2).y(height / 2))
-    .restart();
-
-};
-
-//#region SVG1
-// Create the SVG container for the first node network
-let svg1 = d3.create("svg")
-  .attr("width", width)
-  .attr("height", height);
-
-function updateCanvas1(data) {
   // Define the data for the nodes and links
   let nodes_data = data.nodes
   let links_data = data.links
-
-  console.log("links: ", links_data);
 
   // Create the graph layout
   layout = d3.forceSimulation(nodes_data)
@@ -375,57 +303,14 @@ function updateCanvas1(data) {
     .restart();
 
   // Create the node circles and link lines
-  let link = createLinks(links_data, svg1);
-  let node = createNodes(nodes_data, svg1);
+  let link = createLinks(links_data, svg);
+  let node = createNodes(nodes_data, svg);
 
   // Call the function to add labels to the nodes (and make it global)
-  var labels = createLabels(nodes_data, svg1);
+  var labels = createLabels(nodes_data, svg);
 
   // Update the positions of the nodes, links, and labels on each tick of the simulation
   layout.on("tick", function () {
-    link.attr("x1", function (d) { return d.source.x; })
-      .attr("y1", function (d) { return d.source.y; })
-      .attr("x2", function (d) { return d.target.x; })
-      .attr("y2", function (d) { return d.target.y; });
-
-    // Update the node and make it impossible to go out of the svg area
-    node.attr("cx", function (d) { return d.x = Math.max(10, Math.min(width - 50, d.x)); })
-      .attr("cy", function (d) { return d.y = Math.max(10, Math.min(height - 10, d.y)); });
-
-    // Update the label and make it switch side if node is at the end of the right half
-    labels.attr("x", function (d) { return (d.x < (width / 8 * 7)) ? d.x : (d.x - 80); })
-      .attr("y", function (d) { return d.y; });
-  });
-}
-
-// Create another SVG container for the second node network
-let svg2 = d3.create("svg")
-  .attr("width", width)
-  .attr("height", height);
-
-function updateCanvas2(data) {
-  // Define the data for the second set of nodes and links
-  let nodes_data = data.nodes;
-  let links_data = data.links;
-
-  // Create the second graph layout
-  layout2 = d3.forceSimulation(nodes_data)
-    .force("charge", d3.forceManyBody().strength(-height / 2))
-    .force("link", d3.forceLink(links_data).distance(100))
-    .force("center", d3.forceCenter().x(width / 2).y(height / 2))
-    .restart();
-
-  // Call the function to create the links for svg2
-  let link = createLinks(links_data, svg2);
-
-  // Create the second node circles
-  let node = createNodes(nodes_data, svg2);
-
-  // Add labels to the second nodes
-  let labels = createLabels(nodes_data, svg2)
-
-  // Update the positions of the nodes, links, and labels on each tick of the simulation
-  layout2.on("tick", function () {
     link.attr("x1", function (d) { return d.source.x; })
       .attr("y1", function (d) { return d.source.y; })
       .attr("x2", function (d) { return d.target.x; })
@@ -439,12 +324,14 @@ function updateCanvas2(data) {
     labels.attr("x", function (d) { return (d.x < (width / 8 * 7)) ? d.x : (d.x - 80); })
       .attr("y", function (d) { return d.y; });
   });
+
 }
 
-//#endregion
-
-// Import all the episodes
-importAllEpisodes();
+// Create the SVG containers for the two node network
+let svg1 = d3.create("svg")
+  .attr("width", width)
+  .attr("height", height);
+let svg2 = svg1.clone(true);
 
 // Append the SVG element.
 document.getElementById("container").append(svg1.node());
